8397d3a7454029574cd601319d8111cd98bf453c
park0372 <krc8372@gmail.com>
2026-08-08 11:25:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -272,35 +272,6 @@ if (ivipMenuBtn && ivipMenu) {
     });
 
 }
-// ============================================
-// IVIP Logo Menu
-// ============================================
-
-const ivipMenuBtn = document.getElementById("ivipMenuBtn");
-const ivipMenu = document.getElementById("ivipMenu");
-
-if (ivipMenuBtn && ivipMenu) {
-
-    ivipMenuBtn.addEventListener("click", function (e) {
-
-        e.stopPropagation();
-
-        ivipMenu.classList.toggle("show");
-
-    });
-
-    document.addEventListener("click", function (e) {
-
-        if (!ivipMenu.contains(e.target) &&
-            !ivipMenuBtn.contains(e.target)) {
-
-            ivipMenu.classList.remove("show");
-
-        }
-
-    });
-
-}
 
 
 
